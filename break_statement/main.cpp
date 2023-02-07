42ca3487402cb582b65b9dc2b29d410edbddfd45
htrq <119476945+htrq@users.noreply.github.com>
2023-02-07 13:00:21 +0300

    break example shown

--- break_statement/main.cpp
+++ break_statement/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// The function is to find just the only one value in the array
+void findInArrayFunction(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            cout << "Found element: " << key << " at " << i << " position" << endl;
+            break;
+        }
+    }
+}
+
 int main(int, char**) {
-    cout << "Hello, world!" << endl;
+    int arr[]{ 10, 20, 30, 40, 30, 50 };
+    findInArrayFunction(arr, 6, 30);
     return 0;
 }
