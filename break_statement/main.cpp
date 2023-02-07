9e2acd458495242f8d6a450c3c3c7e408063256d
htrq <119476945+htrq@users.noreply.github.com>
2023-02-07 13:09:53 +0300

    made example with continue statement

--- break_statement/main.cpp
+++ break_statement/main.cpp
@@ -11,8 +11,22 @@ void findInArrayFunction(int arr[], int size, int key) {
     }
 }
 
+void printArrayOnlyWithoutTheOneNumber(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            continue;
+        }
+        else {
+            cout << arr[i] << endl;
+        }
+    }
+
+}
+
 int main(int, char**) {
     int arr[]{ 10, 20, 30, 40, 30, 50 };
     findInArrayFunction(arr, 6, 30);
+    printArrayOnlyWithoutTheOneNumber(arr, 6, 30);
+
     return 0;
 }
