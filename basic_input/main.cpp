a458e3defe93929e78034b1a4a1af0bb07ac8629
htrq <119476945+htrq@users.noreply.github.com>
2023-02-10 10:53:19 +0300

    added example of clog and cerr

---
+++ basic_input/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(int, char**) {
+    cerr << "an error occured.." << endl;
+    clog << "an error occured even from clog.." << endl;
+    return 0;
+}
